40582e71be9375d7680786be01bdf5b09fec9418
Kevin GUILLARD <stolcraft@gmail.com>
2020-04-23 17:06:35 +0200

    daooooooooooooo

--- src/main/java/eu/ensup/gestionscolairespringboot/dao/IEtudiantDao.java
+++ src/main/java/eu/ensup/gestionscolairespringboot/dao/IEtudiantDao.java
@@ -3,12 +3,14 @@ package eu.ensup.gestionscolairespringboot.dao;
 
 import eu.ensup.gestionscolairespringboot.domaine.Etudiant;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 
 public interface IEtudiantDao extends JpaRepository<Etudiant, Integer>{
     
-     public void saveStudent(Etudiant etu);
- 
+     Etudiant findById(String id);
     
 
 }
