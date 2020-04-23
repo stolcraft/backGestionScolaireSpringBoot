0f39554f495af27a047abb3fb9f2de751bcf46ca
Kevin GUILLARD <stolcraft@gmail.com>
2020-04-23 15:04:22 +0200

    package

---
+++ src/main/java/eu/ensup/gestionscolairespringboot/dao/IEtudiantDao.java
@@ -0,0 +1,5 @@
+package eu.ensup.gestionscolairespringboot.dao;
+
+public interface IEtudiantDao {
+
+}
---
+++ src/main/java/eu/ensup/gestionscolairespringboot/service/EtudiantService.java
@@ -0,0 +1,5 @@
+package eu.ensup.gestionscolairespringboot.service;
+
+public class EtudiantService {
+
+}
