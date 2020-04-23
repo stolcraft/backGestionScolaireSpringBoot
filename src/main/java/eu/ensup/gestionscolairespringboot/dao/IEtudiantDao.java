155c4aa905a8fa2ddbcdf21b79d2c43b10e639e3
maxime20091996 <chevalierm96@gmail.com>
2020-04-23 16:15:02 +0200

    commit

--- src/main/java/eu/ensup/gestionscolairespringboot/dao/IEtudiantDao.java
+++ src/main/java/eu/ensup/gestionscolairespringboot/dao/IEtudiantDao.java
@@ -1,14 +1,6 @@
 package eu.ensup.gestionscolairespringboot.dao;
 
 import eu.ensup.gestionscolairespringboot.domaine.Etudiant;
-import java.util.Lit;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-	
-}
-package eu.ensup.gestionscolairespringboot.dao;
-
-import eu.ensup.gestionscolairespringboot.domaine.Etudiant;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
