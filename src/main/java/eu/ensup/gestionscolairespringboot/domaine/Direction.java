01cc81d1717154b9642d45434a0d03dd1312f949
Kevin GUILLARD <stolcraft@gmail.com>
2020-04-23 14:58:03 +0200

    kkk

---
+++ src/main/java/eu/ensup/gestionscolairespringboot/domaine/Direction.java
@@ -0,0 +1,47 @@
+package eu.ensup.gestionscolairespringboot.domaine;
+import javax.persistence.Entity;
+
+/**
+ * @author benja
+ * Classe Direction h�ritant de la classe Personne
+ */
+@Entity
+public class Direction extends Personne {
+
+	private String login;
+	private String password;
+	
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public Direction(String nom, String prenom, String mail, String adresse, int telephone) {
+		super(nom, prenom, mail, adresse, telephone);
+	}
+	
+	public Direction(String nom, String prenom, String mail, String adresse, int telephone, String login,
+			String password) {
+		super(nom, prenom, mail, adresse, telephone);
+		this.login = login;
+		this.password = password;
+	}
+	
+	public Direction(String login, String password) {
+		super();
+		this.login = login;
+		this.password = password;
+	}
+	
+	public Direction() {
+		super();
+	}
+}
---
+++ src/main/java/eu/ensup/gestionscolairespringboot/presentation/GestionscolairespringbootApplication.java
@@ -0,0 +1,13 @@
+package eu.ensup.gestionscolairespringboot.presentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionscolairespringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionscolairespringbootApplication.class, args);
+	}
+
+}
